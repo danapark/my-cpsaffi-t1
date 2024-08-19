a88b500368a6ce0289d43b3a3db67eae5d0cf5b2
danapark <dana.parkth@gmail.com>
2024-08-19 17:10:47 +0900

    search error on index.html, try2

--- app/web_scraper.py
+++ app/web_scraper.py
@@ -1,6 +1,10 @@
 import requests
+import random
+import urllib3
+from urllib3.util.retry import Retry
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, parse_qs, urlencode
+from requests.adapters import HTTPAdapter
 
 def convert_to_affiliate_link(original_url, channel_id, partners_code):
     parsed_url = urlparse(original_url)
@@ -23,6 +27,25 @@ def convert_to_affiliate_link(original_url, channel_id, partners_code):
     
     return f"https://link.coupang.com/re/AFFSDP?{urlencode(affiliate_params)}"
 
+def requests_retry_session(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
+
 def search_coupang(query, num_products=10, sort_by="", channel_id="", partners_code=""):
     base_url = "https://www.coupang.com/np/search"
     params = {
@@ -37,13 +60,18 @@ def search_coupang(query, num_products=10, sort_by="", channel_id="", partners_c
     }
     url = f"{base_url}?{urlencode(params)}"
     
+    user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'
+    ]
+
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "User-Agent": random.choice(user_agents),
         "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
     }
-    
     try:
-        response = requests.get(url, headers=headers, timeout=10)
+        response = requests_retry_session().get(url, headers=headers, timeout=30)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, 'html.parser')
         
